a141512e0222fe063ba695c9f36783a90c5d8433
jonas-amilton <jonas96@edu.unisinos.br>
2024-11-27 22:09:01 -0300

    feat: adicionado rota para servir as imagens para consumo no frontend do app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import cors from "cors";
 import sequelize from "./database/database";
 import authRoutes from "./routes/authRoutes";
@@ -15,6 +16,10 @@ app.use(express.json());
 app.use("/api/pet-connect/auth", authRoutes);
 app.use("/api/pet-connect", userRoutes);
 app.use("/api/pet-connect", petRoutes);
+app.use(
+  "/api/pet-connect/images",
+  express.static(path.join(__dirname, "../uploads/images"))
+);
 
 // Conecta ao banco de dados
 sequelize
@@ -30,4 +35,5 @@ app.listen(3000, () => {
   console.log(
     `Servidor rodando localmente em ${"http://localhost:3000/api/pet-connect"}`
   );
+  console.log("Servindo imagens de:", path.join(__dirname, "/uploads/images"));
 });
