bd6365977ae4e31710bf09983255b1683f6f5c22
jonas-amilton <jonas96@edu.unisinos.br>
2024-12-04 01:53:12 -0300

    refactor: alterado configurações do multer para salvar imagens no banco de dados em mimetype

--- src/config/multerConfig.ts
+++ src/config/multerConfig.ts
@@ -1,15 +1,22 @@
 import multer from "multer";
-import path from "path";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../../uploads/images"));
+const storage = multer.memoryStorage();
+
+const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
+  const allowedTypes = ["image/jpeg", "image/png"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Tipo de arquivo não suportado. Permitido: JPEG, PNG"));
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
+  fileFilter,
 });
-
-const upload = multer({ storage });
 
 export default upload;
